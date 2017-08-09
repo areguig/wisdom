afbb5c2a69f3bd4ba371253737b4e5377badc439
Akli Reguig <akli.reguig@gmail.com>
2017-08-09 23:35:18 +0200

    Added twitter buttons

--- js/quotes.js
+++ js/quotes.js
@@ -1,12 +1,28 @@
+const T_TWEET = "https://twitter.com/intent/tweet?text=";
+const T_RETWEET = "https://twitter.com/intent/retweet?tweet_id=";
+const T_LIKE ="https://twitter.com/intent/like?tweet_id=";
 
 var play = true;
 $('#play').show();
+$('#t_like').hide(); 
+$('#t_retweet').hide(); 
+$('#t_tweet').hide(); 
 
 display_quote = function () {
     if (play) {
         var quote = quotes[Math.floor(Math.random() * quotes.length)];
         $('#quote').html(quote.quote);
         $('#author').html('— ' + quote.author)
+        var t_quote ='"'+quote.quote+'"'+'-' +(quote.twitter_handle?'@'+quote.twitter_handle:quote.author);
+        $('#t_tweet').attr("href", T_TWEET+t_quote);
+        $('#t_tweet').show()
+        if(quote.tweet_id){
+            $('#t_retweet').attr("href", encodeURI(T_RETWEET+''+quote.tweet_id));
+            $('#t_retweet').show()
+            $('#t_like').attr("href", encodeURI(T_LIKE+''+quote.tweet_id));
+            $('#t_like').show()
+
+        }
     }
 }
 
